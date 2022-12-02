d3a26a9e35ea4c5ea8fcefd0cf3d2cc77e8ea1a3
TVPRO20 <66882914+TVPRO20@users.noreply.github.com>
2022-12-02 02:49:25 -0500

    initial commit

--- js/canales.js
+++ js/canales.js
@@ -7,29 +7,29 @@ class Menu extends HTMLElement {
     
     <nav class="flex-container d-lg-flex">
     <nav class="flex-container d-lg-flex ">
-        <a href="https://peliculasplaya.netlify.app/IPTV.html ">
+        <a href="http://peliculasplaya.netlify.app/IPTV.html ">
             <img src="/imagenes/music-top.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV2.html ">
+        <a href="http://peliculasplaya.netlify.app/IPTV2.html ">
             <img src="/imagenes/cnn-es.webp " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV3.html ">
+        <a href="http://peliculasplaya.netlify.app/IPTV3.html ">
             <img src="/imagenes/Mliga.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV4.html ">
+        <a href="http://peliculasplaya.netlify.app/IPTV4.html ">
             <img src="/imagenes/foxsport2.png " class="img-thumbnail " alt="... " class="w-50 ">
-        <a href="https://peliculasplaya.netlify.app/IPTV5.html ">
+        <a href="http://peliculasplaya.netlify.app/IPTV5.html ">
             <img src="/imagenes/bein-sport-extra.webp " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV6.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV6.html " target="_blank">
             <img src="/imagenes/Espn2.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV7.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV7.html " target="_blank">
             <img src="/imagenes/beIN-Sports-LaLiga.webp " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV8.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV8.html " target="_blank">
             <img src="/imagenes/Tntsport.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV9.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV9.html " target="_blank">
             <img src="/imagenes/clarosport.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV10.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV10.html " target="_blank">
             <img src="/imagenes/Sky-bundes-liga.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV11.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV11.html " target="_blank">
             <img src="/imagenes/sky-sports-premier-league.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-        <a href="https://peliculasplaya.netlify.app/IPTV12.html " target="_blank">
+        <a href="http://peliculasplaya.netlify.app/IPTV12.html " target="_blank">
             <img src="/imagenes/nfl.png " class="img-thumbnail " alt="... " class="w-50 "></a>                            
     </nav>
 </nav>
