77a59a09d4966fb4547f1a024bc719cbc5c737ee
TVPRO20 <66882914+TVPRO20@users.noreply.github.com>
2022-12-23 23:34:51 -0500

    sport23

--- js/canales.js
+++ js/canales.js
@@ -12,13 +12,13 @@ class Menu extends HTMLElement {
         <a href="http://peliculasplaya.netlify.app/IPTV2.html ">
             <img src="/imagenes/cnn-es.webp " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV3.html ">
-            <img src="/imagenes/Mliga.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/Aztecauno.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV4.html ">
             <img src="/imagenes/foxsport2.png " class="img-thumbnail " alt="... " class="w-50 ">
         <a href="http://peliculasplaya.netlify.app/IPTV5.html ">
             <img src="/imagenes/bein-sport-extra.webp " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV6.html " target="_blank">
-            <img src="/imagenes/Espn2.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/SportsNews.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV7.html " target="_blank">
             <img src="/imagenes/beIN-Sports-LaLiga.webp " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV8.html " target="_blank">
@@ -26,9 +26,9 @@ class Menu extends HTMLElement {
         <a href="http://peliculasplaya.netlify.app/IPTV9.html " target="_blank">
             <img src="/imagenes/clarosport.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV10.html " target="_blank">
-            <img src="/imagenes/Sky-bundes-liga.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/Tennis-Channel.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV11.html " target="_blank">
-            <img src="/imagenes/sky-sports-premier-league.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/FoxDeportes.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://peliculasplaya.netlify.app/IPTV12.html " target="_blank">
             <img src="/imagenes/nfl.png " class="img-thumbnail " alt="... " class="w-50 "></a>                            
     </nav>
