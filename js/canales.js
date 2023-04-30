989ea95ad2d1396d8550f4225ee47dd01b51ec72
JIMJEAM <jimmyjeam99@gmail.com>
2023-04-30 00:10:36 -0500

    box

--- js/canales.js
+++ js/canales.js
@@ -38,33 +38,33 @@ class Menu extends HTMLElement {
         <a href="http://pelisplaya.net/IPTV14.html " target="_blank">
             <img src="/imagenes/foxsport1.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV15.html " target="_blank">
-            <img src="/imagenes/Sonychannel.png " class="img-thumbnail " alt="... " class="w-50 "></a> 
+            <img src="/imagenes/twp.png " class="img-thumbnail " alt="... " class="w-50 "></a> 
         <a href="http://pelisplaya.net/IPTV16.html " target="_blank">
-            <img src="/imagenes/Paramount.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/surfchannel.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV17.html " target="_blank">
-            <img src="/imagenes/Adrenalinasport.png " class="img-thumbnail " alt="... " class="w-50 "></a> 
+            <img src="/imagenes/tucine.png " class="img-thumbnail " alt="... " class="w-50 "></a> 
         <a href="http://pelisplaya.net/IPTV18.html " target="_blank">
-            <img src="/imagenes/Starchannel.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/novelasco.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV19.html " target="_blank">
-            <img src="/imagenes/Unicable.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/foro.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV20.html " target="_blank">
-            <img src="/imagenes/Distritocomedia.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/amas.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV21.html " target="_blank">
-            <img src="/imagenes/golden.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/multimediosmty.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV22.html " target="_blank">
-            <img src="/imagenes/goldenplus.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/topcine.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV23.html " target="_blank">
-            <img src="/imagenes/goldenedge.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/horrorfy.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV24.html " target="_blank">
-            <img src="/imagenes/cinecanal.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/cinesureno.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV25.html " target="_blank">
-            <img src="/imagenes/Fx.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/fightnetwork.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV26.html " target="_blank">
-            <img src="/imagenes/Ae.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/glory.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV27.html " target="_blank">
-            <img src="/imagenes/Amc.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/hardknocks.png " class="img-thumbnail " alt="... " class="w-50 "></a>
         <a href="http://pelisplaya.net/IPTV28.html " target="_blank">
-            <img src="/imagenes/Space.png " class="img-thumbnail " alt="... " class="w-50 "></a>
+            <img src="/imagenes/impact.png " class="img-thumbnail " alt="... " class="w-50 "></a>
             <a href="http://pelisplaya.net/IPTV29.html " target="_blank">
             <img src="/imagenes/GenTv.png " class="img-thumbnail " alt="... " class="w-50 "></a>
 </nav>
