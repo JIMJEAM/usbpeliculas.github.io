4d56f594333f9622afdf3407e234651986ed822c
jimjeam <79390776+JIMJEAM@users.noreply.github.com>
2023-12-22 15:22:07 -0500

    123

--- js/pelis.js
+++ js/pelis.js
@@ -17,8 +17,8 @@ class Menu extends HTMLElement {
     <ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
 
        
-    <li class="cursor-pointer"> <img  src="https://image.tmdb.org/t/p/w500/zwcmjn3qKhP7kUmpgNT0f61xzU3.jpg" alt="
-    Radical (2023)" class="img_video_nav  img-thumbnail  w-50"  onclick="videoUrl('https://megaup.net/21tgn/4qu4m4n.4nd.th3.l0st.k1ngd0m.2023.hc.cam.latino.mp4')">
+    <li class="cursor-pointer"> <img  src="https://static.cinepolis.com/img/peliculas/43451/1/1/43451.jpg" alt="
+    Trolls 3: Se Armó La Banda (2023)" class="img_video_nav  img-thumbnail  w-50"  onclick="videoUrl('https://notabasica.com/612132/Trolls%203%20-%20Se%20Arm%C3%B3%20la%20Banda.mp4')">
     </li>
 
     <li class="cursor-pointer"> <img  src="https://www.lahiguera.net/cinemania/pelicula/10653/los_asesinos_de_la_luna-cartel-11388.jpg" alt="Los asesinos de la luna (2023)" class="img_video_nav img-thumbnail   w-50" onclick="videoUrl('https://grrfff66me7t.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grrfff66me7t/b/Cubojoselyn/o/reset%2Fpeliculas%2Foctubre%2FVer%20Los%20asesinos%20de%20la%20luna%20online%20HD%20-%20Cuevana%202.mp4')"></li>
