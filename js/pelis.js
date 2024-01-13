64f659d3d84759c07d165c119afa9bca035179e5
jimjeam <79390776+JIMJEAM@users.noreply.github.com>
2024-01-13 12:26:21 -0500

    123play

--- js/pelis.js
+++ js/pelis.js
@@ -16,7 +16,7 @@ class Menu extends HTMLElement {
 
     <ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
 
-    <li class="cursor-pointer"> <img src="/imagenes/Aquaman Y El Reino Perdido.jpg" alt="Aquaman y el reino perdido (2023) " class="img_video_nav img-thumbnail w-50" onclick="videoUrl('https://cdnst38.tokyvideo.com/videos/381/381741/mp4/a1e814ce843becd5ce6b23ba30c10afc9633c7bdee736a8b144ce7a8f7cfef25.mp4?secure=F60PLrxIup5uOaUkpQTU-A%3D%3D%2C1705183392')"> </li> 
+    <li class="cursor-pointer"> <img src="/imagenes/Aquaman Y El Reino Perdido.jpg" alt="Aquaman y el reino perdido (2023) " class="img_video_nav img-thumbnail w-50" onclick="videoUrl('https://cdnst50.tokyvideo.com/videos/457/457764/mp4/0cd206c9b00e92162e724ac8502094bc723f25d1ea09663a7961b4917196269a.mp4?secure=_xwWEPk0qqdQ3cEIO_NzDA%3D%3D%2C1705251957')"> </li> 
 
        
     <li class="cursor-pointer"> <img  src="https://www.serantes.com/wp-content/uploads/2023/10/SKATROLLS3.jpg" alt="
