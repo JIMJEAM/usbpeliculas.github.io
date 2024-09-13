4144f9ab04274fea98941061a27eb71ba433f708
jimjeam <66882914+TVPRO20@users.noreply.github.com>
2024-09-12 20:19:44 -0500

    update

--- js/series247.js
+++ js/series247.js
@@ -17,15 +17,14 @@ class Menu extends HTMLElement {
     <nav class="flex-container">
     <nav class="flex-container row">
     <nav class="flex-container col-lg-12 m-auto">
+        
+        <a href="series24-7.html ">
+            <img src="https://www.apkvisit.com/wp-content/uploads/2022/10/anime-tv-anime-watching-app.png " class="img-thumbnail " alt="... " class="w-50 "></a>
 
          <a href="series24-7retro.html ">
             <img src="https://m.media-amazon.com/images/I/B1d2i--qjtS._SL500_.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-           
-        <a href="series24-7anime.html ">
-            <img src="https://www.apkvisit.com/wp-content/uploads/2022/10/anime-tv-anime-watching-app.png " class="img-thumbnail " alt="... " class="w-50 "></a>
-
         
-        <a href="series24-7.html ">
+        <a href="series24-7twitch.html ">
             <img src="../imagenes/twitch247.png" class="img-thumbnail " alt="... " class="w-50 "></a>
             
             <a href="series24-7series2.html ">
